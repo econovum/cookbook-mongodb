513a31fdf23ddb42e1146df4b09f2d285fc2d788
Andreas Simon <econovum@andreas-simon.info>
2012-07-12 17:15:56 +0200

    Added default recipe and RHEL repo template

---
+++ recipes/default.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+case node[:platform]
+when "redhat", "centos", "fedora"
+  template "/etc/yum.repos.d/10gen.repo"
+
+  execute "yum -y update"
+  execute "yum -y install mongo-10gen mongo-10gen-server"
+when "debian", "ubuntu"
+  include_recipe "apt"
+
+  apt_repository "10gen" do
+    action :add
+    init_process = node[:platform] == "debian" || node[:lsb][:codename] <= "karmic" ? "debian-sysvinit" : "ubuntu-upstart"
+    uri "deb http://downloads-distro.mongodb.org/repo/#{init_process} "
+    distribution node['lsb']['codename']
+    components %w(dist 10gen)
+    keyserver "keyserver.ubuntu.com"
+    key "7F0CEB10"
+  end
+
+  package "mongodb-10gen" do
+    action :install
+  end
+else
+  raise "Platform #{node[:platform]} is not supported yet!"
+end
