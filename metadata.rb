b135fcd3234315fbb34bb5e28473c8b229b40be5
Andreas Simon <econovum@andreas-simon.info>
2012-07-24 21:24:10 +0200

    Added metadata

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+maintainer       "Eco Novum"
+maintainer_email "chef@econovum.de"
+license          "Apache 2.0"
+description      "Installs MongoDB from the 10gen repository"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
